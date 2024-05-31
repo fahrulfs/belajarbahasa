9fc401aaf252a89063d6e5f175292706b7b806d5
fs <fahrulfajarsaputro@gmail.com>
2024-05-31 11:31:31 +0700

    add component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-  fontFamily: {
-    'mainFont':["Rubik", "sans-serif"],
+      fontFamily: {
+        'mainFont': ["Inter", "sans-serif"],
         'customFont': ["Poppins", "sans-serif"],
-    
-      }
+      },
+      gridTemplateColumns: {
+        'auto-fill': 'repeat(auto-fill, minmax(200px, 1fr))',
+        'auto-fit': 'repeat(auto-fit, minmax(200px, auto))',
+      },
+        colors: {
+          'main': '#e1a90c',
+          'second': '#f4c237',
+          'accent': '#056aad',
+          'accent2': '#fae3a4'
+            
+          // Configure your color palette here
+        }
+  
     },
   },
   plugins: [
